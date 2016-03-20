3bc4019cbc5e46b393838ddf68dd525565189a45
frendon <frendonq@gmail.com>
2016-03-20 21:39:39 +0100

    First commit. Basic structure.

---
+++ Esp8266-open-sdk.mk
@@ -0,0 +1,145 @@
+########################################################################
+
+ROOT       = /home/nando/tools/esp-open-sdk
+BUILD_BASE = build
+FW_BASE    = firmware
+
+# Base directory for the compiler
+XTENSA_TOOLS_ROOT ?= $(ROOT)/xtensa-lx106-elf/bin
+
+# base directory of the ESP8266 SDK package, absolute
+SDK_BASE	?= $(ROOT)/sdk
+
+#Esptool.py path and port
+ESPTOOL		?= $(ROOT)/esptool/esptool.py
+ESPPORT		?= /dev/ttyUSB0
+
+# name for the target project
+TARGET		= app
+
+# which modules (subdirectories) of the project to include in compiling
+MODULES      = driver user
+EXTRA_INCDIR = include $(ROOT)/include/
+
+# libraries used in this project, mainly provided by the SDK
+LIBS = c gcc hal phy net80211 lwip wpa upgrade ssl main pp
+
+# compiler flags using during compilation of source files
+# Pedantic style
+#CFLAGS       = -Os -g -O2 -Wpointer-arith -Wundef -Werror -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals  -D__ets__ -DICACHE_FLASH -pedantic -Wall -Wextra
+CFLAGS       = -Os -g -O2 -Wpointer-arith -Wundef -Werror -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals  -D__ets__ -DICACHE_FLASH
+CXXFLAGS     = $(CFLAGS) -fno-rtti -fno-exceptions
+FLAGS_STD   = -std=gnu11
+CXXFLAGS_STD = -std=gnu++11
+
+# linker flags used to generate the main object file
+LDFLAGS	= -nostdlib -Wl,--no-check-sections -u call_user_start -Wl,-static
+
+# linker script used for the above linkier step
+LD_SCRIPT	= eagle.app.v6.ld
+
+# various paths from the SDK used in this project
+SDK_LIBDIR	= lib
+SDK_LDDIR	= ld
+SDK_INCDIR	= include include/json
+
+# we create two different files for uploading into the flash
+# these are the names and options to generate them
+FW_FILE_1      = 0x00000
+FW_FILE_2      = 0x40000
+
+# select which tools to use as compiler, librarian and linker
+CC		:= $(XTENSA_TOOLS_ROOT)/xtensa-lx106-elf-gcc
+CXX		:= $(XTENSA_TOOLS_ROOT)/xtensa-lx106-elf-g++
+AR		:= $(XTENSA_TOOLS_ROOT)/xtensa-lx106-elf-ar
+LD		:= $(XTENSA_TOOLS_ROOT)/xtensa-lx106-elf-gcc
+
+####
+#### no user configurable options below here
+####
+
+SRC_DIR       := $(MODULES)
+BUILD_DIR     := $(addprefix $(BUILD_BASE)/,$(MODULES))
+
+SDK_LIBDIR    := $(addprefix $(SDK_BASE)/,$(SDK_LIBDIR))
+SDK_INCDIR    := $(addprefix -I$(SDK_BASE)/,$(SDK_INCDIR))
+
+SRC           := $(foreach sdir,$(SRC_DIR),$(wildcard $(sdir)/*.c*))
+C_OBJ         := $(patsubst %.c,%.o,$(SRC))
+CXX_OBJ       := $(patsubst %.cpp,%.o,$(C_OBJ))
+OBJ           := $(patsubst %.o,$(BUILD_BASE)/%.o,$(CXX_OBJ))
+LIBS          := $(addprefix -l,$(LIBS))
+APP_AR        := $(addprefix $(BUILD_BASE)/,$(TARGET)_app.a)
+TARGET_OUT    := $(addprefix $(BUILD_BASE)/,$(TARGET).out)
+
+LD_SCRIPT     := $(addprefix -T$(SDK_BASE)/$(SDK_LDDIR)/,$(LD_SCRIPT))
+
+INCDIR        := $(addprefix -I,$(SRC_DIR))
+EXTRA_INCDIR  := $(addprefix -I,$(EXTRA_INCDIR))
+MODULE_INCDIR := $(addsuffix /include,$(INCDIR))
+
+FW_FILE_1     := $(addprefix $(FW_BASE)/,$(FW_FILE_1).bin)
+FW_FILE_2     := $(addprefix $(FW_BASE)/,$(FW_FILE_2).bin)
+
+V ?= $(VERBOSE)
+ifeq ("$(V)","1")
+Q :=
+vecho := @true
+else
+Q := @
+vecho := @echo
+endif
+
+vpath %.c $(SRC_DIR)
+vpath %.cpp $(SRC_DIR)
+
+define compile-objects
+$1/%.o: %.c
+	$(vecho) "CC $$<"
+	$(Q) $(CC) $(INCDIR) $(MODULE_INCDIR) $(EXTRA_INCDIR) $(SDK_INCDIR) $(CFLAGS)  -c $$< -o $$@
+$1/%.o: %.cpp
+	$(vecho) "C+ $$<"
+	$(Q) $(CXX) $(INCDIR) $(MODULE_INCDIR) $(EXTRA_INCDIR) $(SDK_INCDIR) $(CXXFLAGS)  -c $$< -o $$@
+endef
+
+.PHONY: all checkdirs clean
+
+all: checkdirs $(TARGET_OUT) $(FW_FILE_1) 
+
+$(FW_FILE_1): $(TARGET_OUT)
+	$(vecho) "FW $@"
+	$(Q) $(ESPTOOL) elf2image $(TARGET_OUT) --output $(FW_BASE)/
+
+
+$(TARGET_OUT): $(APP_AR)
+	$(vecho) "LD $@"
+	$(Q) $(LD) -L$(SDK_LIBDIR) $(LD_SCRIPT) $(LDFLAGS) -Wl,--start-group $(LIBS) $(APP_AR) -Wl,--end-group -o $@
+
+$(APP_AR): $(OBJ)
+	$(vecho) "AR $@"
+	$(Q) $(AR) cru $@ $^
+
+checkdirs: $(BUILD_DIR) $(FW_BASE)
+
+$(BUILD_DIR):
+	$(Q) mkdir -p $@
+
+firmware:
+	$(Q) mkdir -p $@
+
+
+flash: all
+	esptool.py write_flash 0 $(FW_FILE_1) 0x40000 $(FW_FILE_2)
+
+clean:
+	$(Q) rm -f $(APP_AR)
+	$(Q) rm -f $(TARGET_OUT)
+	$(Q) rm -rf $(BUILD_DIR)
+	$(Q) rm -rf $(BUILD_BASE)
+
+
+	$(Q) rm -f $(FW_FILE_1)
+	$(Q) rm -f $(FW_FILE_2)
+	$(Q) rm -rf $(FW_BASE)
+
+$(foreach bdir,$(BUILD_DIR),$(eval $(call compile-objects,$(bdir))))
---
+++ README.md
@@ -0,0 +1,6 @@
+# makefile-esp8266
+
+This is an example of makefile for blinky project using esp-open-sdk.
+
+The arduino-ide is buggy and slow  to upload the firmware to the device.
+
